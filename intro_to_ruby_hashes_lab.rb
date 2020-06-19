a1de65f4b464daa0dbec2183bbdcf90c4f7efd72
Bradley Calkins <b.raders1013@gmail.com>
2020-06-19 03:23:57 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,18 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
 
+  monopoly =  {
+  :railroads=> {}
+  }
 end
 
+    
+  
+
+  # Remember implicit return! We're returning this new Hash _without_ the
+  # keyword "return." Nice and neat.
 def monopoly_with_second_tier
+  hash = { :railroads=> {:pieces=> 4, :rent_in_dollars=> {}, :names=>{} } } 
+  
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -13,6 +21,24 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+  hash = { 
+    :railroads=> {
+      :pieces=> 4, 
+      :rent_in_dollars=> {
+        :one_piece_owned=> 25,
+        :two_pieces_owned=> 50,
+        :three_pieces_owned=> 100,
+        :four_pieces_owned=> 200
+      }, 
+      :names=>{
+        :reading_railroad=> {},
+        :pennsylvania_railroad=> {},
+        :shortline_railroad=> {},
+        :b_and_o_railroad=> {},
+      } 
+      
+    }
+  }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -21,9 +47,34 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
+  
+   hash = { 
+    :railroads=> {
+      :pieces=> 4, 
+      :rent_in_dollars=> {
+        :one_piece_owned=> 25,
+        :two_pieces_owned=> 50,
+        :three_pieces_owned=> 100,
+        :four_pieces_owned=> 200
+      }, 
+      :names=>{
+        :reading_railroad=> {:mortgage_value=> 100},
+        :pennsylvania_railroad=> {:mortgage_value=> 200},
+        :shortline_railroad=> {:mortgage_value=> 800},
+        :b_and_o_railroad=> {:mortgage_value=> 400},
+      } 
+      
+    }
+  }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
 end
+
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+
